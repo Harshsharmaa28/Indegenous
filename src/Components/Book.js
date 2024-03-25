7b3331387f6b11c7cf56f1d5bae171d35ba34747
Harsh Sharma <harshsharma0828@gmail.com>
2024-03-26 00:04:09 +0530

    fixed bug

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,6 +1,6 @@
 const Book = ({data}) => {
     return (
-        <div className="border p-5 rounded-lg">
+        <div className="border p-5 rounded-lg text-sm">
             <h3 className="text-black font-semibold text-lg">
                 {data.title}
             </h3>
